914a42d8e487ba2905518b5783bb8842bc8c48cb
ryanemitchell <ryanmitchell@gmail.com>
2020-06-12 12:27:33 +0000

    build for release_Object

--- vendor/mrmrs/pesticide/src/model/javascript.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  source: './data/templates/template.js',
-  target: './js/pesticide.js',
-  entry: '\t\t\t\'%s\': \'%s\','
-};
--- vendor/mrmrs/pesticide/src/model/less.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  source: './data/templates/template.less',
-  target: './less/pesticide.less',
-  entry: '\t%s { outline: 1px solid %s !important; }'
-};
